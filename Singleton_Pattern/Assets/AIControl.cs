d2c0e52066028e7e5f0f49fab8f95e4e59d2b964
Celil Eren Kalkan <60541083+CelilErenKalkan@users.noreply.github.com>
2021-12-19 18:58:06 +0300

    Singleton Class Implemented
    The Singleton Class implemented and used without putting it in the scene.

--- Singleton_Pattern/Assets/AIControl.cs
+++ Singleton_Pattern/Assets/AIControl.cs
@@ -5,7 +5,6 @@ using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour {
 
-	GameObject[] goalLocations;
 	UnityEngine.AI.NavMeshAgent agent;
     Animator anim;
     Vector3 lastGoal;
@@ -13,8 +12,7 @@ public class AIControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		goalLocations = GameObject.FindGameObjectsWithTag("goal");
-		agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
         anim = GetComponent<Animator>();
         anim.SetBool("isWalking", true);
         PickGoalLocation();
@@ -23,7 +21,7 @@ public class AIControl : MonoBehaviour {
     void PickGoalLocation()
     {
         lastGoal = agent.destination;
-        agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        agent.SetDestination(GameEnvironment.Singleton.RandomGoal().transform.position);
     }
 
 	
@@ -32,6 +30,19 @@ public class AIControl : MonoBehaviour {
         if (agent.remainingDistance < 1) //At the goal
         {
             PickGoalLocation();
+        }
+
+        foreach (GameObject go in GameEnvironment.Singleton.Obstacles)
+        {
+            float distance = Vector3.Distance(go.transform.position, this.transform.position);
+            if (distance < 5 && Random.Range(0,100) < 5)
+            {
+                agent.SetDestination(lastGoal);
+            }
+            else if (distance < 1)
+            {
+                GameEnvironment.Singleton.RemoveObstacles(go);
+            }
         }
 	}
 }
---
+++ Singleton_Pattern/Assets/GameEnvironment.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnvironment
+{
+    private static GameEnvironment instance;
+    private List<GameObject> obstacles = new List<GameObject>();
+    private List<GameObject> goalsList = new List<GameObject>();
+
+    public List<GameObject> Obstacles
+    {
+        get { return obstacles; }
+    }
+    public List<GameObject> GoaList
+    {
+        get { return goalsList; }
+    }
+
+    public static GameEnvironment Singleton
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEnvironment();
+                instance.GoaList.AddRange(GameObject.FindGameObjectsWithTag("goal"));
+            }
+
+            return instance;
+        }
+    }
+
+    public void AddObstacles(GameObject go)
+    {
+        obstacles.Add(go);
+    }
+
+    public void RemoveObstacles(GameObject go)
+    {
+        int index = obstacles.IndexOf(go);
+        obstacles.RemoveAt(index);
+        GameObject.Destroy(go);
+    }
+
+    public GameObject RandomGoal()
+    {
+        return goalsList[Random.Range(0, goalsList.Count)];
+    }
+}
